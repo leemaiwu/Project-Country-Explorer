558a819f11d7c45669f2f58968ad4ed96173c38e
Laura Wu <leemaiwu@gmail.com>
2023-05-10 21:57:35 -0600

    Initial commit

---
+++ src/Components/OptionDisplay.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { useSelector } from "react-redux"
+import { selectPotentials } from "../redux/slices/potentialCountriesSlice"
+
+const OptionDisplay = () => {
+
+    let currentPotentials = useSelector(selectPotentials)
+
+    return <div className="stack">
+                {currentPotentials.map((e, i) => {
+                    return (
+                        <h2 key={i} className="country-option">
+                            {e.name.common}
+                        </h2>
+                    )
+                })
+                }
+            </div>
+}
+
+export default OptionDisplay
