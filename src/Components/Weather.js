fc61a3a1df3d41f2dd047c66eb2079ee4c6e93d6
Laura Wu <leemaiwu@gmail.com>
2023-05-12 13:11:14 -0600

    Completed project. Connected weather api

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,30 +1,59 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { selectDisplay } from "../redux/slices/displayCountrySlice"
 
 const Weather = () => {
     const [weather, setWeather] = useState()
 
-    // ------------------------------------
-    // PASTE RAPIDAPI CODE SNIPPET IN A USEEFFECT HERE
-    // ------------------------------------
+    const display = useSelector(selectDisplay)
+    const latitude = display.capitalInfo.latlng[0]
+    const longitude = display.capitalInfo.latlng[1]
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            url: 'https://weatherapi-com.p.rapidapi.com/current.json',
+            params: {q: `${latitude}, ${longitude}`},
+            headers: {
+                'X-RapidAPI-Key': 'd731cd16dbmshb8b9f063fbfab0ep1d1d18jsn837c6ae196ac',
+                'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+            }
+        }
+
+        axios
+            .request(options)
+            .then((res) => {
+                setWeather(res.data)
+                console.table(res.data)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }, [latitude, longitude])
 
     return (
         <div>
             <table className="overview-table">
                 <tr>
                     <td>Conditions: </td>
+                    <td>{weather?.current?.condition?.text}</td>
                 </tr>
                 <tr>
                     <td>Temperature: </td>
+                    <td>{weather?.current?.temp_f}</td>
                 </tr>
                 <tr>
                     <td>Feels Like: </td>
+                    <td>{weather?.current?.feelslike_f}</td>
                 </tr>
                 <tr>
                     <td>Humidity: </td>
+                    <td>{weather?.current?.humidity}</td>
                 </tr>
                 <tr>
                     <td>Wind Speed: </td>
+                    <td>{weather?.current?.wind_mph} mph</td>
                 </tr>
             </table>
         </div>
